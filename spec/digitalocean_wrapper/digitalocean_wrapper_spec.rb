f31440a764f12750d0145681550d11a39186ca2c
Pavel Lobashov <shockwavenn@gmail.com>
2015-05-05 13:36:39 +0300

    add test for DigitalOceanWrapper#get_image_id_by_name

--- spec/digitalocean_wrapper/digitalocean_wrapper_spec.rb
+++ spec/digitalocean_wrapper/digitalocean_wrapper_spec.rb
@@ -6,4 +6,9 @@ describe DigitalOceanWrapper do
     digital_ocean = DigitalOceanWrapper.new
     expect(digital_ocean.client.access_token).not_to be_empty
   end
+
+  it 'get_image_id_by_name' do
+    digital_ocean = DigitalOceanWrapper.new
+    expect(digital_ocean.get_image_id_by_name('nct-at-stable')).to be_a(Fixnum)
+  end
 end
