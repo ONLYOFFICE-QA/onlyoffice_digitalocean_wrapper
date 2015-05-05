129cb57c60d80791746f3bc1bd761aff7ec10a95
Pavel Lobashov <shockwavenn@gmail.com>
2015-05-05 13:50:17 +0300

    1. add test for DigitalOceanWrapper#get_droplet_status_by_name 2. start using let for rspec

--- spec/digitalocean_wrapper/digitalocean_wrapper_spec.rb
+++ spec/digitalocean_wrapper/digitalocean_wrapper_spec.rb
@@ -2,28 +2,33 @@ require 'rspec'
 require_relative '../../testing_shared'
 
 describe DigitalOceanWrapper do
+  let (:digital_ocean) { DigitalOceanWrapper.new }
+
   it 'check for correct load access token from file' do
-    digital_ocean = DigitalOceanWrapper.new
     expect(digital_ocean.client.access_token).not_to be_empty
   end
 
   it 'get_image_id_by_name' do
-    digital_ocean = DigitalOceanWrapper.new
     expect(digital_ocean.get_image_id_by_name('nct-at-stable')).to be_a(Fixnum)
   end
 
   it 'get_droplet_by_name with existing name' do
-    digital_ocean = DigitalOceanWrapper.new
     expect(digital_ocean.get_droplet_by_name('testrail')).to be_a(Fixnum)
   end
 
   it 'get_droplet_by_name with non-existing name' do
-    digital_ocean = DigitalOceanWrapper.new
     expect(digital_ocean.get_droplet_by_name('not testrail')).to be_nil
   end
 
   it 'get_droplet_ip_by_name' do
-    digital_ocean = DigitalOceanWrapper.new
     expect(digital_ocean.get_droplet_ip_by_name('testrail')).to eq('107.170.125.157')
+  end
+
+  it 'get_droplet_status_by_name running droplet' do
+    expect(digital_ocean.get_droplet_status_by_name('testrail')).to eq('active')
+  end
+
+  it 'get_droplet_status_by_name nonexisting droplet' do
+    expect(digital_ocean.get_droplet_status_by_name('not testrail')).to be_nil
   end
 end
