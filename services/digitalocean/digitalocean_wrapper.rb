1e8314e57852f5d6bb0631539037c74a071bd3e1
Pavel Lobashov <shockwavenn@gmail.com>
2015-05-18 15:02:19 +0300

    fix change_kernel and rename

--- services/digitalocean/digitalocean_wrapper.rb
+++ services/digitalocean/digitalocean_wrapper.rb
@@ -44,7 +44,7 @@ class DigitalOceanWrapper
     ip
   end
 
-  def get_droplet_kernel_by_name(droplet_name)
+  def current_kernel(droplet_name)
     droplets = @client.droplets.all
     droplet = droplets.find { |x| x.name == droplet_name }
     kernel_name = droplet.kernel.name
@@ -86,9 +86,9 @@ class DigitalOceanWrapper
 
   def change_kernel(droplet_name, kernel_name)
     droplet_id = get_droplet_by_name(droplet_name)
-    all_kernels = kernels_of_droplet(droplet_id)
-    needed_kernel_id = all_kernels.select{ |cur_kernel| cur_kernel.name == kernel_name}
-    client.droplet_actions.change_kernel(droplet_id: droplet_id, kernel: kernel_id)
+    all_kernels = kernels_of_droplet(droplet_name)
+    needed_kernel_id = all_kernels.select{ |cur_kernel| cur_kernel.name == kernel_name}.first.id
+    client.droplet_actions.change_kernel(droplet_id: droplet_id, kernel: needed_kernel_id)
   end
 
   def restore_image_by_name(image_name = 'nct-at-stable', droplet_name = image_name)
