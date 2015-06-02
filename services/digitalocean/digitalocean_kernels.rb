581f182218c34d122a184e33302abd98239dedbd
Oleg <nazarov90@gmail.com>
2015-06-02 17:23:22 +0300

    added images and kernels

---
+++ services/digitalocean/digitalocean_kernels.rb
@@ -0,0 +1,42 @@
+class DigitalOceanKernels
+  class << self
+
+    def ubuntu_14_04_x64
+      ['Ubuntu 14.04 x64 vmlinuz-3.13.0-24-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-27-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-29-generic',
+       'Ubuntu 14.04 x64 vmlinuz-3.13.0-30-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-32-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-33-generic',
+       'Ubuntu 14.04 x64 vmlinuz-3.13.0-34-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-35-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-36-generic',
+       'Ubuntu 14.04 x64 vmlinuz-3.13.0-37-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-39-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-40-generic',
+       'Ubuntu 14.04 x64 vmlinuz-3.13.0-43-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-44-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-45-generic',
+       'Ubuntu 14.04 x64 vmlinuz-3.13.0-46-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-48-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-49-generic',
+       'Ubuntu 14.04 x64 vmlinuz-3.13.0-51-generic', 'Ubuntu 14.04 x64 vmlinuz-3.13.0-52-generic']
+    end
+
+    def ubuntu_14_10_x64
+      ['Ubuntu 14.10 x64 vmlinuz-3.16.0-21-generic', 'Ubuntu 14.10 x64 vmlinuz-3.16.0-23-generic', 'Ubuntu 14.10 x64 vmlinuz-3.16.0-28-generic',
+       'Ubuntu 14.10 x64 vmlinuz-3.16.0-29-generic', 'Ubuntu 14.10 x64 vmlinuz-3.16.0-30-generic', 'Ubuntu 14.10 x64 vmlinuz-3.16.0-31-generic',
+       'Ubuntu 14.10 x64 vmlinuz-3.16.0-33-generic', 'Ubuntu 14.10 x64 vmlinuz-3.16.0-34-generic', 'Ubuntu 14.10 x64 vmlinuz-3.16.0-37-generic']
+    end
+
+    def ubuntu_15_04_x64
+      ['Ubuntu 15.04 x64 vmlinuz-3.19.0-15-generic', 'Ubuntu 15.04 x64 vmlinuz-3.19.0-15-generic', 'Ubuntu 15.04 x64 vmlinuz-3.19.0-16-generic']
+    end
+
+    def centos_7_x64
+      ['CentOS 7 x64 vmlinuz-3.10.0-123.13.1.el7.x86_64', 'CentOS 7 x64 vmlinuz-3.10.0-123.13.2.el7.x86_64', 'CentOS 7 x64 vmlinuz-3.10.0-123.20.1.el7.x86_64',
+       'CentOS 7 x64 vmlinuz-3.10.0-123.4.2.el7.x86_64', 'CentOS 7 x64 vmlinuz-3.10.0-123.6.3.el7.x86_64', 'CentOS 7 x64 vmlinuz-3.10.0-123.8.1.el7.x86_64',
+       'CentOS 7 x64 vmlinuz-3.10.0-123.9.2.el7.x86_64', 'CentOS 7 x64 vmlinuz-3.10.0-123.9.3.el7.x86_64', 'CentOS 7 x64 vmlinuz-3.10.0-123.el7.x86_64',
+       'CentOS 7 x64 vmlinuz-3.10.0-229.1.2.el7.x86_64', 'CentOS 7 x64 vmlinuz-3.10.0-229.4.2.el7.x86_64']
+    end
+
+    def debian_7
+      ['Debian 7.0 x64 vmlinuz-3.2.0-4-amd64  (3.2.41-2) ', 'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.54-2)', 'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.57-3)',
+       'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.57-3+deb7u1)', 'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.57-3+deb7u2)',
+       'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.60-1+deb7u1)', 'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.60-1+deb7u3)',
+       'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.63-2)', 'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.63-2+deb7u1)',
+       'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.63-2+deb7u2)', 'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.65-1)',
+       'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.65-1+deb7u1)', 'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.65-1+deb7u2)',
+       'Debian 7.0 x64 vmlinuz-3.2.0-4-amd64 (3.2.68-1+deb7u1)']
+    end
+
+  end
+end
