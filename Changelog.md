7c04624165dadfadfa2c19785b7e60092413b8f3
Pavel Lobashov <shockwavenn@gmail.com>
2017-12-22 14:06:55 +0300

    Add ability to retry request if error happened

--- Changelog.md
+++ Changelog.md
@@ -4,6 +4,7 @@
 * Add check of droplet size for creation
 * Do not force 2.0.1 of `droplet_kit`
 * Change default region to `nyc3`
+* Add ability to retry request if error happened
 
 # 0.2.0
 * Force use ruby 2.2 to be able use `activesupport 5` 
---
+++ lib/onlyoffice_digitalocean_wrapper/digitalocean_wrapper/exceptions_retryer.rb
@@ -0,0 +1,23 @@
+module OnlyofficeDigitaloceanWrapper
+  # Module to ignore DigitalOcean exception if they happen irregularly
+  module ExceptionsRetryer
+    # Retry if exception happened
+    # @param exception [Exception] exception to retry
+    # @param retries [Integer] how much to retry
+    # @param timeout [Integer] wait until next retry
+    def retry_exception(exception = DropletKit::Error,
+                        retries: 5,
+                        timeout: 10)
+      try = 0
+      begin
+        yield
+      rescue exception => exp
+        try += 1
+        OnlyofficeLoggerHelper.log("Error '#{exception}, #{exp}' happened during "\
+                                   "operation. Retrying #{try} of #{retries}")
+        sleep timeout # Time to cooldown error
+        try <= retries ? retry : raise
+      end
+    end
+  end
+end
