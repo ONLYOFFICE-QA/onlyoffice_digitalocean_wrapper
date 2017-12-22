9d01d0927a3acd234b74dc55e2626a8e3c0f1ca6
Pavel Lobashov <shockwavenn@gmail.com>
2017-12-22 14:56:41 +0300

    Remove testrail and use bugzilla in tests

--- spec/droplet_data_get_spec.rb
+++ spec/droplet_data_get_spec.rb
@@ -1,37 +1,39 @@
 require 'spec_helper'
 
 describe 'DigitalOceanWrapper get droplet data' do
+  existing_droplet = 'bugzilla.onlyoffice.com'
+  nonexisting_droplet = 'not bugzilla'
   let(:digital_ocean) { OnlyofficeDigitaloceanWrapper::DigitalOceanWrapper.new }
 
   it 'get_droplet_by_name with existing name' do
-    expect(digital_ocean.droplet_by_name('testrail')).to be_a(DropletKit::Droplet)
+    expect(digital_ocean.droplet_by_name(existing_droplet)).to be_a(DropletKit::Droplet)
   end
 
   it 'get_droplet_by_name with non-existing name' do
-    expect(digital_ocean.droplet_by_name('not testrail')).to be_nil
+    expect(digital_ocean.droplet_by_name(nonexisting_droplet)).to be_nil
   end
 
   it 'get_droplet_id_by_name with existing name' do
-    expect(digital_ocean.get_droplet_id_by_name('testrail')).to be_a(Integer)
+    expect(digital_ocean.get_droplet_id_by_name(existing_droplet)).to be_a(Integer)
   end
 
   it 'get_droplet_id_by_name with non-existing name' do
-    expect(digital_ocean.get_droplet_id_by_name('not testrail')).to be_nil
+    expect(digital_ocean.get_droplet_id_by_name(nonexisting_droplet)).to be_nil
   end
 
   it 'get_droplet_ip_by_name' do
-    expect(digital_ocean.get_droplet_ip_by_name('testrail')).to eq('107.170.125.157')
+    expect(digital_ocean.get_droplet_ip_by_name(existing_droplet)).to eq('104.131.2.120')
   end
 
   it 'get_droplet_ip_by_name non existing name' do
-    expect(digital_ocean.get_droplet_ip_by_name('not-exists')).to be_nil
+    expect(digital_ocean.get_droplet_ip_by_name(nonexisting_droplet)).to be_nil
   end
 
   it 'get_droplet_status_by_name running droplet' do
-    expect(digital_ocean.get_droplet_status_by_name('testrail')).to eq('active')
+    expect(digital_ocean.get_droplet_status_by_name(existing_droplet)).to eq('active')
   end
 
   it 'get_droplet_status_by_name non-existing droplet' do
-    expect(digital_ocean.get_droplet_status_by_name('not testrail')).to be_nil
+    expect(digital_ocean.get_droplet_status_by_name(nonexisting_droplet)).to be_nil
   end
 end
